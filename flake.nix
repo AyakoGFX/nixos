86be48e5fc0b3dfefa9661ba51e4f821f7accd77
Ayako <ayako.gfx@gmail.com>
2025-03-03 20:33:41 +0530

    z

--- flake.nix
+++ flake.nix
@@ -7,38 +7,31 @@
     home-manager = {
       url = "github:nix-community/home-manager/release-24.11";
       # The `follows` keyword in inputs is used for inheritance.
-      # Here, `inputs.nixpkgs` of home-manager is kept consistent with
-      # the `inputs.nixpkgs` of the current flake,
-      # to avoid problems caused by different versions of nixpkgs.
       inputs.nixpkgs.follows = "nixpkgs";
     };
   };
 
   outputs = inputs@{ nixpkgs, home-manager, ... }: {
     nixosConfigurations = {
-      # TODO please change the hostname to your own mine is nixos
+      # Change the hostname to your own
       nixos = nixpkgs.lib.nixosSystem {
         system = "x86_64-linux";
         modules = [
           ./configuration.nix
 
-          # make home-manager as a module of nixos
-          # so that home-manager configuration will be deployed automatically when executing `nixos-rebuild switch`
+          # Make home-manager a module of nixos
           home-manager.nixosModules.home-manager
           {
             home-manager.useGlobalPkgs = true;
             home-manager.useUserPackages = true;
 
-            # TODO replace ayako with your own username
+            # Replace 'ayako' with your own username
             home-manager.users.ayako = import ./home.nix;
-
-            # Optionally, use home-manager.extraSpecialArgs to pass arguments to home.nix
           }
         ];
       };
     };
   };
 }
-
 
 # https://gitlab.com/Clsmith1/nixos/-/blob/main/home.nix?ref_type=heads  
