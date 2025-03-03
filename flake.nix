49c1ed9fef2ca2b4a75a68cac4ade15963f9366f
Ayako <ayako.gfx@gmail.com>
2025-03-03 16:45:37 +0530

    Update flake.nix

--- flake.nix
+++ flake.nix
@@ -1,29 +1,30 @@
 {
-  description = "install home manager";
-  
+  description = "my first flake";
+
   inputs = {
     nixpkgs.url = github:NixOS/nixpkgs/nixos-unstable;
     home-manager.url = github:nix-community/home-manager/master;
     home-manager.inputs.nixpkgs.follows = "nixpkgs";
   };
-  outputs = {self, nixpkgs, home-manager, ...}:
+
+  outputs = { self, nixpkgs, home-manager, ... }:
     let
       lib = nixpkgs.lib;
       system = "x86_64-linux";
-      pkg = nixpkgs.legacyPackages.${system};
+      pkgs = nixpkgs.legacyPackages.${system};
     in
-      {
-        nixosConfiguration ={
-          nixos = lib.nixosSystem {
-            inherit system;
-            modules = [./configuration.nix];
-          };
-        };
-        homeConfiguration = {
-          chris = home-manager.lib.homeManagerConfiguration {
-            inherit pkgs;
-            modules = [./home.nix];
-          };
+    {
+      nixosConfigurations = {
+        GhostNix = lib.nixosSystem {
+          inherit system;
+          modules = [ ./configuration.nix ];
         };
       };
+      homeConfigurations = {
+        chris = home-manager.lib.homeManagerConfiguration {
+          inherit pkgs;
+          modules = [ ./home.nix ];
+        };
+      };
+    };
 }
