a4cf55d0b1f873c8690fff8fc109129813dd1399
Ayako <ayako.gfx@gmail.com>
2025-10-04 19:19:06 +0530

    z

--- flake.nix
+++ flake.nix
@@ -1,37 +1,13 @@
 {
-  description = "NixOS configuration with Home Manager";
+  description = "A very basic flake";
 
   inputs = {
-    # nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
-    nixpkgs-stable.url = "github:NixOS/nixpkgs/nixos-25.05";
-    # nur.url = "github:nix-community/NUR";
-    
-    home-manager = {
-      url = "github:nix-community/home-manager";
-      inputs.nixpkgs.follows = "nixpkgs";
-    };
+    nixpkgs.url = "github:nixos/nixpkgs?ref=nixos-unstable";
   };
 
-  outputs = { self, nixpkgs, home-manager, ... }@inputs: {
-    nixosConfigurations = {
-      # Replace 'your-hostname' with your actual hostname
-      nixos = nixpkgs.lib.nixosSystem {
-        system = "x86_64-linux";
-        modules = [
-          ./configuration.nix
-          
-          # Home Manager module
-          home-manager.nixosModules.home-manager
-          {
-            home-manager.useGlobalPkgs = true;
-            home-manager.useUserPackages = true;
-            home-manager.users.ayako = import ./home.nix;
-            
-            # Optionally, use extraSpecialArgs to pass arguments to home.nix
-            # home-manager.extraSpecialArgs = { inherit inputs; };
-          }
-        ];
+  outputs = { self, nixpkgs }: {
+    nixosConfigurations.nixos = nixpkgs.lib.nixosSystem {
+      modules = [ ./configuration.nix ];
       };
     };
-  };
-}
+  }
--- modules/desktops/gnome.nix
+++ modules/desktops/gnome.nix
@@ -1,8 +1,10 @@
 { config, pkgs, lib, ... }:
 
 {
-  services.desktopManager.gnome.enable = true;
-  services.displayManager.gdm.enable = true;
+  services.xserver.desktopManager.gnome.enable = true;
+  services.xserver.displayManager.gdm.enable = true;
+  # services.desktopManager.gnome.enable = true;
+  # services.displayManager.gdm.enable = true;
   environment.systemPackages = with pkgs.gnomeExtensions; [
     blur-my-shell
     launch-new-instance
